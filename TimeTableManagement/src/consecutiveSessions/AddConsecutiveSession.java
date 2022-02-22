3995dbf9cebc960e802d430c97113fba9b18a5b6
Muhasheikh <muhasheikh8@gmail.com>
2022-02-22 17:49:50 +0530

    Add Consecutive Session

---
+++ TimeTableManagement/src/consecutiveSessions/AddConsecutiveSession.java
@@ -0,0 +1,46 @@
+package consecutiveSessions;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+import connection.DbConnection;
+
+public class AddConsecutiveSession {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		DbConnection.connect();
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AddConsecutiveSession window = new AddConsecutiveSession();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public AddConsecutiveSession() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
